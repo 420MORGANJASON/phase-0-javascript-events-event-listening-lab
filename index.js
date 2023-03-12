0f092ef654f46bce8812f1849b3937316aaebe75
MORGANJASON <420morganjason@gmail.com>
2023-03-12 14:20:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-function addingEventListener() {
+const input= documentgetElementById('input');
+
+function clickAlert() {
+    alert('I was clicked!');
 }
+input.addingEventListener('click', clickAlert("Hee hee, that tickles!"));
+{console.log ("clickAlert")}
+
+
+
+
+
+//const input = document.getElementById('input');
+//input.addEventListener('click', function() {
+ // alert('I was clicked!');
+//});
